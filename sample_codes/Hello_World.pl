59b4271aac768fc79aea1b45795c31d453d9360c
Tushar Semwal <tushar92.iitg@gmail.com>
2018-03-12 23:31:59 +0530

    Update Hello_World.pl

--- sample_codes/Hello_World.pl
+++ sample_codes/Hello_World.pl
@@ -2,15 +2,15 @@
 %Author: Tushar Semwal (semwaltushar@gmail.com)
 
 
-a_static_agent_handler(guid,(IP,Port),main):-
+a_static_agent_handler(guid,(_IP,_Port),main):-
         writeln('Hello World!'),!.
         
         
-        
-%Steps to create and execute the static agent
-
+%=========================================================%
+%------Steps to create and execute the static agent-------%
+%=========================================================%
 % 1. Start a Tartarus platform on a port. e.g. start_tartarus(localhost,12121).
 % 2. Consult the Hello_World.pl file. e.g. consult('path_to_file/Hello_World.pl'). OR if you are in the same directory, just enter consult('Hello_World.pl').
 % 3. Create a static agent with a given (or random) name at a port DIFFERENT FROM TARTARUS PORT. e.g. create_static_agent(myagent,(localhost,33333),a_static_agent_handler).
-% 4. Execute the agent using: execute_agent(myagent,(localhost,12121),a_static_agent_handler).
+% 4. Execute the agent using: execute_agent(myagent,(localhost,33333),a_static_agent_handler). NOTE:the port is same as that of Static Agent and NOT that of Tartarus. This is differnt in the case of mobile agents.
 % 5. That's all!
